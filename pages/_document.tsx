26b52dbadf7066d2d3252ed3d02026cd94779cba
oyi77 <oyi77@users.noreply.github.com>
2026-03-28 16:07:06 +0700

    fix: Update GTM ID to GTM-5MXPQQRT (correct Meta account)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,7 +12,7 @@ export default function Document() {
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TH94NJH3');`,
+})(window,document,'script','dataLayer','GTM-5MXPQQRT');`,
           }}
         />
         {/* End Google Tag Manager */}
@@ -160,7 +160,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-TH94NJH3"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5MXPQQRT"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
